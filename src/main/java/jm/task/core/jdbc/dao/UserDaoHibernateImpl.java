85b2d4072e6fda4ada80305f86c813dda76adf02
Rashotik <95523846+Rashotik@users.noreply.github.com>
2022-02-09 13:48:58 +0300

    commit6

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,6 +1,10 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 
 import java.util.List;
 
@@ -11,26 +15,61 @@ public class UserDaoHibernateImpl implements UserDao {
     }
 
     public void createUsersTable() {
+        Session session = Util.getSessionFactory().openSession();
+        Transaction transaction = session.beginTransaction();
 
+        String sql = "CREATE TABLE IF NOT EXISTS `user` " +
+                "(`id` BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY, " +
+                "`name` VARCHAR(50) NOT NULL, " +
+                "`lastName` VARCHAR(50) NOT NULL, " +
+                "`age` INT NOT NULL)";
+
+        Query query = session.createSQLQuery(sql).addEntity(User.class);
+        query.executeUpdate();
+        transaction.commit();
+        session.close();
     }
 
     public void dropUsersTable() {
+        Session session = Util.getSessionFactory().openSession();
+        Transaction transaction = session.beginTransaction();
 
+        String sql = "DROP TABLE IF EXISTS user";
+
+        Query query = session.createSQLQuery(sql).addEntity(User.class);
+        query.executeUpdate();
+        transaction.commit();
+        session.close();
     }
 
     public void saveUser(String name, String lastName, byte age) {
-
+        Session session = Util.getSessionFactory().openSession();
+        Transaction transaction = session.beginTransaction();
+        session.save(new User(name, lastName, age));
+        transaction.commit();
+        session.close();
     }
 
     public void removeUserById(long id) {
-
+        Session session = Util.getSessionFactory().openSession();
+        Transaction tx1 = session.beginTransaction();
+        session.delete(session.get(User.class, id));
+        tx1.commit();
+        session.close();
     }
 
     public List<User> getAllUsers() {
-        return null;
+        return (List<User>) Util.getSessionFactory()
+                .openSession()
+                .createQuery("FROM User")
+                .list();
     }
 
     public void cleanUsersTable() {
-
+        Session session = Util.getSessionFactory().openSession();
+        Transaction transaction = session.beginTransaction();
+        session.createQuery("DELETE FROM User").executeUpdate();
+        transaction.commit();
+        session.close();
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,9 +1,14 @@
 package jm.task.core.jdbc.util;
+import java.util.Properties;
+
+import jm.task.core.jdbc.model.User;
+import org.hibernate.SessionFactory;
 
 import java.sql.*;
 
 public class Util {
     static private Connection connect = null;
+    static private SessionFactory sessionFactory = null;
 
     static public Connection getConnect (){
         if(connect == null) {
@@ -15,5 +20,28 @@ public class Util {
             }
         }
         return connect;
+    }
+    public static SessionFactory getSessionFactory() {
+        if(sessionFactory == null) {
+            try{
+                Properties prop = new Properties();
+                prop.setProperty("hibernate.connection.url", "jdbc:mysql://localhost:3306/my");
+                prop.setProperty("hibernate.connection.username", "root");
+                prop.setProperty("hibernate.connection.password", "rashot-8937561");
+                prop.setProperty("dialect", "org.hibernate.dialect.MySQL8Dialect");
+
+                prop.setProperty("hibernate.hbm2ddl.auto", "create");
+
+                sessionFactory = new org.hibernate.cfg.Configuration()
+                        .addProperties(prop)
+                        //.addPackage("com.kat")
+                        .addAnnotatedClass(User.class)
+                        .buildSessionFactory()
+                ;
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+        return sessionFactory;
     }
 }
